bd8facd9868778da22adcc465052d52c85e1f8c5
NduvhoEdward <Nduvho.Ramashia@gmail.com>
2024-04-30 12:15:55 +0200

    Start of LDC1614 modulation

---
+++ Core/Inc/LDC1614.h
@@ -0,0 +1,13 @@
+/*
+ * LDC1614.h
+ *
+ *  Created on: Apr 30, 2024
+ *      Author: BatanayiGwanyanya
+ */
+
+#ifndef INC_LDC1614_H_
+#define INC_LDC1614_H_
+
+
+
+#endif /* INC_LDC1614_H_ */
---
+++ Core/Src/LDC1614.c
@@ -0,0 +1,8 @@
+/*
+ * LDC1614.c
+ *
+ *  Created on: Apr 30, 2024
+ *      Author: BatanayiGwanyanya
+ */
+
+
